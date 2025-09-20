870e7f8adfd4d4b2b94cc9bbd2461bc70390826d
dima1310 <dimaponomarenko13101981@gmail.com>
2025-09-20 21:12:02 +0300

    task-6

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -1,5 +1,10 @@
 import createHttpError from 'http-errors';
-import { createProduct, getAllProducts, getProductById } from '../services/products.js';
+import {
+  createProduct,
+  getAllProducts,
+  getProductById,
+  updateProduct,
+} from '../services/products.js';
 
 export const listProductsController = async (req, res) => {
   const products = await getAllProducts();
@@ -18,11 +23,9 @@ export const getProductByIdController = async (req, res) => {
     throw createHttpError(404, 'Product not found');
   }
   res.status(200).json({
-
     status: 200,
-    message: "Successfully found product with id {productId}!",
+    message: 'Successfully found product with id {productId}!',
     data: product,
-
   });
 };
 
@@ -30,7 +33,20 @@ export const createProductController = async (req, res) => {
   const product = await createProduct(req.body);
   res.status(201).json({
     status: 201,
-    message: "Successfully created a product!",
+    message: 'Successfully created a product!',
+    data: product,
+  });
+};
+
+export const updateProductController = async (req, res) => {
+  const { productId } = req.params;
+  const product = await updateProduct(productId, req.body);
+  if (!product) {
+    throw createHttpError(404, 'Product not found');
+  }
+  res.status(200).json({
+    status: 200,
+    message: 'Successfully patched a product!',
     data: product,
   });
 };
